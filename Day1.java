3cc92b7d725973326bd117e9897655fc209b0083
GiteshGayakwad <143732488+GiteshGayakwad@users.noreply.github.com>
2025-01-13 18:08:32 +0530

    palindrome problem added

--- Day1.java
+++ Day1.java
@@ -1,3 +1,38 @@
 // Palindrom Number
 
+import java.util.Scanner;
+
+public class Day1 {
+    public static void main(String[] args) {
+        
+        Scanner scn = new Scanner(System.in);
+
+        int input = scn.nextInt();
+
+        int n = input;
+        // int temp = input;
+        int revNumber = 0;
+
+        if(n < 0){
+            System.out.println(false);
+        }
+
+        while (n > 0) {
+            
+            int digit = n % 10;
+            
+            revNumber = revNumber * 10 + digit;
+
+            n = n/10;
+
+        }
+
+        if(revNumber == input){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+        scn.close();
+    }
+}
 
